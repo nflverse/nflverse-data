ba55967a9d0477f9161eb9ad2631b20feb3e80af
Tan-DESKPC <tan@tanho.ca>
2022-03-06 00:51:02 -0500

    add json file of timestamp for shields.io

---
+++ NEWS.md
@@ -0,0 +1,8 @@
+# nflversedata 0.0.2
+
+* Added a `NEWS.md` file to track changes to the package.
+* Added timestamp.json to experiment with shields.io badges
+
+# nflversedata 0.0.1
+
+* Added data upload wrapper function (`nflverse_upload()`) including timestamp updates.
--- R/upload.R
+++ R/upload.R
@@ -17,20 +17,25 @@ nflverse_upload <- function(files, tag, ...){
 update_release_timestamp <- function(tag){
   x <- tempdir(check = TRUE)
   on.exit(file.remove(file.path(x,"timestamp.txt")), add = TRUE)
-  update_time <- Sys.time()
-  writeLines(as.character(update_time), file.path(x,"timestamp.txt"))
+
+  update_time <- format(Sys.time(),tz = "America/Toronto",usetz = TRUE)
+  writeLines(update_time, file.path(x,"timestamp.txt"))
+
+  list(last_updated = update_time) |> jsonlite::toJSON(auto_unbox = TRUE) |> writeLines(file.path(x,"timestamp.json"))
+
   piggyback::pb_upload(file.path(x,"timestamp.txt"), repo = "nflverse/nflverse-data", tag = tag, overwrite = TRUE)
+  piggyback::pb_upload(file.path(x,"timestamp.json"), repo = "nflverse/nflverse-data", tag = tag, overwrite = TRUE)
 
-  current_release <- httr::GET(glue::glue("https://api.github.com/repos/nflverse/nflverse-data/releases/tags/{tag}")) |>
-    httr::content()
-
-  current_body <- current_release$body
-
-  new_body <- gsub("Last Updated: .*$", "",x = current_body) |> paste0("Last Updated: ",update_time)
-
-  update_result <- httr::PATCH(glue::glue("https://api.github.com/repos/nflverse/nflverse-data/releases/{current_release$id}"),
-              httr::add_headers(Authorization = paste("token",gh::gh_token())),
-              body = jsonlite::toJSON(list(body = new_body),auto_unbox = TRUE))
+  # current_release <- httr::GET(glue::glue("https://api.github.com/repos/nflverse/nflverse-data/releases/tags/{tag}")) |>
+  #   httr::content()
+  #
+  # current_body <- current_release$body
+  #
+  # new_body <- gsub("Last Updated: .*$", "",x = current_body) |> paste0("Last Updated: ",update_time)
+  #
+  # update_result <- httr::PATCH(glue::glue("https://api.github.com/repos/nflverse/nflverse-data/releases/{current_release$id}"),
+  #             httr::add_headers(Authorization = paste("token",gh::gh_token())),
+  #             body = jsonlite::toJSON(list(body = new_body),auto_unbox = TRUE))
 
   invisible(NULL)
 }
