e1a88fb34635e7153e8fb1c0fe88110f5325b0f9
Tan-DESKPC <tan@tanho.ca>
2022-05-06 10:55:09 -0400

    nflverse_save

--- R/upload.R
+++ R/upload.R
@@ -10,32 +10,72 @@ nflverse_upload <- function(files, tag, ...){
   # upload files
   piggyback::pb_upload(files, repo = "nflverse/nflverse-data", tag = tag, ...)
   update_release_timestamp(tag)
-
   cli::cli_alert("Uploaded {length(files)} to nflverse/nflverse-data @ {tag} on {Sys.time()}")
 }
 
 update_release_timestamp <- function(tag){
-  x <- tempdir(check = TRUE)
-  on.exit(file.remove(file.path(x,"timestamp.txt")), add = TRUE)
+  temp_dir <- tempdir(check = TRUE)
 
-  update_time <- format(Sys.time(),tz = "America/Toronto",usetz = TRUE)
-  writeLines(update_time, file.path(x,"timestamp.txt"))
+  update_time <- format(Sys.time(), tz = "America/Toronto", usetz = TRUE)
+  writeLines(update_time, file.path(temp_dir, "timestamp.txt"))
 
-  list(last_updated = update_time) |> jsonlite::toJSON(auto_unbox = TRUE) |> writeLines(file.path(x,"timestamp.json"))
+  list(last_updated = update_time) |>
+    jsonlite::toJSON(auto_unbox = TRUE) |>
+    writeLines(file.path(temp_dir,"timestamp.json"))
 
-  piggyback::pb_upload(file.path(x,"timestamp.txt"), repo = "nflverse/nflverse-data", tag = tag, overwrite = TRUE)
-  piggyback::pb_upload(file.path(x,"timestamp.json"), repo = "nflverse/nflverse-data", tag = tag, overwrite = TRUE)
-
-  # current_release <- httr::GET(glue::glue("https://api.github.com/repos/nflverse/nflverse-data/releases/tags/{tag}")) |>
-  #   httr::content()
-  #
-  # current_body <- current_release$body
-  #
-  # new_body <- gsub("Last Updated: .*$", "",x = current_body) |> paste0("Last Updated: ",update_time)
-  #
-  # update_result <- httr::PATCH(glue::glue("https://api.github.com/repos/nflverse/nflverse-data/releases/{current_release$id}"),
-  #             httr::add_headers(Authorization = paste("token",gh::gh_token())),
-  #             body = jsonlite::toJSON(list(body = new_body),auto_unbox = TRUE))
+  piggyback::pb_upload(file.path(temp_dir,"timestamp.txt"), repo = "nflverse/nflverse-data", tag = tag, overwrite = TRUE)
+  piggyback::pb_upload(file.path(temp_dir,"timestamp.json"), repo = "nflverse/nflverse-data", tag = tag, overwrite = TRUE)
 
   invisible(NULL)
+}
+
+#' Save files to nflverse release
+#'
+#' This functions attaches nflverse attributes like type and timestamp, saves
+#' data to a temporary directory in all four of csv, rds, parquet, and qs formats,
+#' and then uploads to nflverse-data repository for a specified release tag.
+#'
+#' @param dataframe dataframe to save
+#' @param filename filename to upload as, without the file extension
+#' @param nflverse_type metadata: name/information to add to data
+#' @param release_tag name of release to upload to
+#' @param gh_token a GitHub token, defaults to gh::gh_token()
+#'
+#' @export
+nflverse_save <- function(dataframe,
+                          filename,
+                          nflverse_type,
+                          release_tag,
+                          .token = gh::gh_token()){
+
+  stopifnot(
+    is.data.frame(dataframe),
+    is.character(filename),
+    is.character(nflverse_type),
+    is.character(release_tag),
+    is.character(gh_token),
+    length(filename) == 1,
+    length(nflverse_type) == 1,
+    length(release_tag) == 1,
+    length(gh_token) == 1
+  )
+
+  attr(dataframe,"nflverse_type") <- nflverse_type
+  attr(dataframe,"nflverse_data") <- Sys.time()
+
+  temp_dir <- tempdir(check = TRUE)
+
+  saveRDS(dataframe,file.path(temp_dir,paste0(filename,".rds")))
+  data.table::fwrite(dataframe, file.path(temp_dir,paste0(filename,".csv")))
+  arrow::write_parquet(dataframe, file.path(temp_dir, paste0(filename,".parquet")))
+  qs::qsave(dataframe,
+            file.path(temp_dir,paste0(filename,".qs")),
+            preset = "custom",
+            algorithm = "zstd_stream",
+            compress_level = 22,
+            shuffle_control = 15)
+
+  filenames <- file.path(temp_dir, paste0(filename,c(".rds",".csv",".parquet",".qs")))
+
+  nflverse_upload(filenames,tag = release_tag, .token = .token)
 }
