bc691f4322ec1fe88649295bd928ed13232c9ccc
mrcaseb <roemerle@gmail.com>
2022-09-28 12:06:50 +0200

    default to true and add quiet option

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,9 @@
 .onLoad <- function(lib, pkg) {
-  if (Sys.getenv("NFLVERSE.UPLOAD.INSIST", "false") == "true"){
+  if (Sys.getenv("NFLVERSE.UPLOAD.INSIST", "true") == "true"){
     pause_base <- Sys.getenv("NFLVERSE.UPLOAD.PAUSE_BASE", 0.05) |> as.numeric()
     pause_min <- Sys.getenv("NFLVERSE.UPLOAD.PASUE_MIN", 1) |> as.numeric()
     max_times <- Sys.getenv("NFLVERSE.UPLOAD.MAX_TIMES", 10) |> as.numeric()
+    quiet <- Sys.getenv("NFLVERSE.UPLOAD.QUIET", TRUE) |> as.logical()
 
     retry_rate <- purrr::rate_backoff(
       pause_base = pause_base,
@@ -12,13 +13,13 @@
 
     assign(
       x = "nflverse_upload",
-      value = purrr::insistently(nflverse_upload, rate = retry_rate),
+      value = purrr::insistently(nflverse_upload, rate = retry_rate, quiet = quiet),
       envir = parent.env(environment())
     )
 
     assign(
       x = "update_release_timestamp",
-      value = purrr::insistently(update_release_timestamp, rate = retry_rate),
+      value = purrr::insistently(update_release_timestamp, rate = retry_rate, quiet = quiet),
       envir = parent.env(environment())
     )
   }
