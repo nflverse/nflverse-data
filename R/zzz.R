d055af4b5ff221c0e0ffbbd8166e227b3947645d
mrcaseb <roemerle@gmail.com>
2022-09-28 12:01:20 +0200

    allow modification of insistent behavior with env variables

--- R/zzz.R
+++ R/zzz.R
@@ -1,15 +1,25 @@
 .onLoad <- function(lib, pkg) {
-  retry_rate <- purrr::rate_backoff(pause_base = 0.05, pause_min = 1, max_times = 10)
+  if (Sys.getenv("NFLVERSE.UPLOAD.INSIST", "false") == "true"){
+    pause_base <- Sys.getenv("NFLVERSE.UPLOAD.PAUSE_BASE", 0.05) |> as.numeric()
+    pause_min <- Sys.getenv("NFLVERSE.UPLOAD.PASUE_MIN", 1) |> as.numeric()
+    max_times <- Sys.getenv("NFLVERSE.UPLOAD.MAX_TIMES", 10) |> as.numeric()
 
-  assign(
-    x = "nflverse_upload",
-    value = purrr::insistently(nflverse_upload, rate = retry_rate),
-    envir = parent.env(environment())
+    retry_rate <- purrr::rate_backoff(
+      pause_base = pause_base,
+      pause_min = pause_min,
+      max_times = max_times
     )
 
-  assign(
-    x = "update_release_timestamp",
-    value = purrr::insistently(update_release_timestamp, rate = retry_rate),
-    envir = parent.env(environment())
+    assign(
+      x = "nflverse_upload",
+      value = purrr::insistently(nflverse_upload, rate = retry_rate),
+      envir = parent.env(environment())
     )
+
+    assign(
+      x = "update_release_timestamp",
+      value = purrr::insistently(update_release_timestamp, rate = retry_rate),
+      envir = parent.env(environment())
+    )
+  }
 }
