deb06323b60db38d00ced1e93da69b27655454bd
Tan Ho <38083823+tanho63@users.noreply.github.com>
2023-08-03 23:19:20 -0400

    add parameter to specify repository name (#27)
    * Update upload.R
    
    * Update zzz.R
    
    * redoc
    
    redoc

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onLoad <- function(lib, pkg) {
   if (Sys.getenv("NFLVERSE.UPLOAD.INSIST", "true") == "true"){
     pause_base <- Sys.getenv("NFLVERSE.UPLOAD.PAUSE_BASE", 0.05) |> as.numeric()
-    pause_min <- Sys.getenv("NFLVERSE.UPLOAD.PASUE_MIN", 1) |> as.numeric()
+    pause_min <- Sys.getenv("NFLVERSE.UPLOAD.PAUSE_MIN", 1) |> as.numeric()
     max_times <- Sys.getenv("NFLVERSE.UPLOAD.MAX_TIMES", 10) |> as.numeric()
     quiet <- Sys.getenv("NFLVERSE.UPLOAD.QUIET", TRUE) |> as.logical()
 
@@ -14,13 +14,13 @@
     assign(
       x = "nflverse_upload",
       value = purrr::insistently(nflverse_upload, rate = retry_rate, quiet = quiet),
-      envir = parent.env(environment())
+      envir = rlang::ns_env("nflversedata")
     )
 
     assign(
       x = "update_release_timestamp",
       value = purrr::insistently(update_release_timestamp, rate = retry_rate, quiet = quiet),
-      envir = parent.env(environment())
+      envir = rlang::ns_env("nflversedata")
     )
   }
 }
