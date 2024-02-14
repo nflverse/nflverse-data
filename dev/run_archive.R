4c0949d3f66072c7d41ccc25e7f5f2388fcc09c4
Sebastian Carl <mrcaseb@gmail.com>
2024-02-14 11:50:29 +0100

    read tag name to archive from env var

--- dev/run_archive.R
+++ dev/run_archive.R
@@ -1,3 +1,13 @@
 pkgload::load_all()
-piggyback::pb_releases("nflverse/nflverse-data")$tag_name |>
-  lapply(nflversedata::nflverse_archive)
+
+tag <- Sys.getenv("NFLVERSE_ARCHIVE_TAG", unset = NA_character_)
+
+if (is.na(tag)){
+  cli::cli_abort("Can't find the release tag to archive!")
+} else {
+  nflversedata::nflverse_archive(release_name = tag)
+}
+
+# Create tag list for workflow file
+# all_tags <- piggyback::pb_releases("nflverse/nflverse-data")$tag_name
+# cli::cli_bullets(paste("-", sort(all_tags)))
