7ee249f3560b475f021d3c797573cd9e1b4b8c8d
mrcaseb <38586519+mrcaseb@users.noreply.github.com>
2024-02-14 21:31:45 +0100

    Archive in multiple jobs (#41)
    * list all tags in matrix
    to trigger separate jobs for each release
    
    * read tag name to archive from env var
    
    * Update run_archive.yaml
    
    * Update run_archive.yaml
    
    * Update run_archive.yaml
    
    * Create test.yaml
    
    * rename step
    
    * Update archive.R
    
    * Make create_release and have the archive step depend on it
    
    * ignore failure to create release (usually because it already exists)
    
    * query tagnames to write them to a jsonfile
    
    * Update run_archive.yaml
    
    ---------
    
    Co-authored-by: Tan Ho <38083823+tanho63@users.noreply.github.com>

--- R/archive.R
+++ R/archive.R
@@ -6,14 +6,18 @@
 nflverse_archive <- function(release_name){
   cli::cli_alert_info("Archiving {release_name}")
   temp_dir <- tempdir()
-  nflreadr::nflverse_download(!!release_name,folder_path = temp_dir, file_type = "rds")
+  nflreadr::nflverse_download(!!release_name, folder_path = temp_dir, file_type = "rds")
 
-  try({
-    piggyback::pb_new_release(
-      repo = "nflverse/nflverse-data-archives",
-      tag = glue::glue("archive-{as.character(Sys.Date())}"))
-  },
-  silent = TRUE)
+  release_tag <- glue::glue("archive-{as.character(Sys.Date())}")
+  if (!release_tag %in% piggyback::pb_releases("nflverse/nflverse-data-archives")$release_name) {
+      
+    try({
+      piggyback::pb_new_release(
+        repo = "nflverse/nflverse-data-archives",
+        tag = glue::glue("archive-{as.character(Sys.Date())}"))
+    },
+    silent = TRUE)
+  }
 
   file_list <- list.files(file.path(temp_dir,release_name))
 
@@ -21,6 +25,7 @@ nflverse_archive <- function(release_name){
     file.path(temp_dir,release_name,file_list),
     file.path(temp_dir,release_name, paste0(release_name,"_",file_list))
   )
+  
   memoise::forget(piggyback::pb_releases)
   memoise::forget(piggyback:::pb_info)
   Sys.sleep(5)
@@ -28,7 +33,7 @@ nflverse_archive <- function(release_name){
   piggyback::pb_upload(
     file = list.files(file.path(temp_dir,release_name),full.names = TRUE),
     repo = "nflverse/nflverse-data-archives",
-    tag = glue::glue("archive-{as.character(Sys.Date())}")
+    tag = release_tag
   )
 
   cli::cli_alert_success("Successfully archived {release_name}")
