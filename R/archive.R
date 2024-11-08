4e3103f10c83f6b313cf79003a906b6abe9cbfc1
Sebastian Carl <mrcaseb@gmail.com>
2024-11-08 16:51:57 +0100

    add archive tag argument to skip release checks

--- R/archive.R
+++ R/archive.R
@@ -1,23 +1,14 @@
 #' Archive nflverse
 #'
 #' @param release_name Name of the release
+#' @param archive_tag The release tag to which data from `release_name` should
+#' be uploaded
 #'
 #' @export
-nflverse_archive <- function(release_name){
+nflverse_archive <- function(release_name, archive_tag){
   cli::cli_alert_info("Archiving {release_name}")
   temp_dir <- tempdir()
   nflreadr::nflverse_download(!!release_name, folder_path = temp_dir, file_type = "rds")
-
-  release_tag <- glue::glue("archive-{as.character(Sys.Date())}")
-  if (!release_tag %in% piggyback::pb_releases("nflverse/nflverse-data-archives")$release_name) {
-      
-    try({
-      piggyback::pb_new_release(
-        repo = "nflverse/nflverse-data-archives",
-        tag = glue::glue("archive-{as.character(Sys.Date())}"))
-    },
-    silent = TRUE)
-  }
 
   file_list <- list.files(file.path(temp_dir,release_name))
 
@@ -25,17 +16,18 @@ nflverse_archive <- function(release_name){
     file.path(temp_dir,release_name,file_list),
     file.path(temp_dir,release_name, paste0(release_name,"_",file_list))
   )
-  
+
   memoise::forget(piggyback::pb_releases)
   memoise::forget(piggyback:::pb_info)
   Sys.sleep(5)
-  
+
+  cli::cli_alert_info("Uploading Files to {.path nflverse/nflverse-data-archives@{archive_tag}}")
   piggyback::pb_upload(
     file = list.files(file.path(temp_dir,release_name),full.names = TRUE),
     repo = "nflverse/nflverse-data-archives",
-    tag = release_tag
+    tag = archive_tag
   )
 
   cli::cli_alert_success("Successfully archived {release_name}")
-  return(invisible(NULL))
+  invisible(NULL)
 }
