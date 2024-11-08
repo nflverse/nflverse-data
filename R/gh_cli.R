8046d80b27814f4656d9a4f6d24cc1855f7fea2f
Sebastian Carl <mrcaseb@gmail.com>
2024-11-08 20:36:23 +0100

    add gh cli implementation of releases and assets

--- R/gh_cli.R
+++ R/gh_cli.R
@@ -70,3 +70,77 @@ gh_cli_release_upload <- function(files,
   invisible(TRUE)
 }
 
+gh_cli_release_tags <- function(repo = "nflverse/nflverse-data"){
+  # see https://cli.github.com/manual/gh_release_list
+
+  # Make sure the gh cli is available
+  gh_cli_available()
+
+  # create command for the shell
+  cli_command <- paste(
+    "gh release list",
+    "-R", repo
+  )
+
+  # This command will error regularly on R error and also errors on warnings
+  # because some failures raise a warning only and we want workflows to fail
+  # if somethings didn't work
+  out <- purrr::quietly(system)(cli_command, intern = TRUE)
+  if (length(out$warnings)) {
+    cli::cli_abort(
+      "The GitHub cli errored with the following message: {.val {out$result}}. \\
+      Here is the R message: {.val {out$warnings}}",
+      call = NULL
+    )
+  }
+
+  out <- strsplit(out$result, "\t", fixed = TRUE)
+  # x[[3]] is the tagName
+  vapply(out, function(x) x[[3]], FUN.VALUE = character(1L))
+}
+
+#' @import data.table
+gh_cli_release_assets <- function(tag, ..., repo = "nflverse/nflverse-data"){
+  # see https://cli.github.com/manual/gh_release_view
+
+  # Make sure the gh cli is available
+  gh_cli_available()
+
+  # create command for the shell
+  cli_command <- paste(
+    "gh release view",
+    tag,
+    "-R", repo,
+    "--json assets"
+  )
+
+  # This command will error regularly on R error and also errors on warnings
+  # because some failures raise a warning only and we want workflows to fail
+  # if somethings didn't work
+  out <- purrr::quietly(system)(cli_command, intern = TRUE)
+  if (length(out$warnings)) {
+    cli::cli_abort(
+      "The GitHub cli errored with the following message: {.val {out$result}}. \\
+      Here is the R message: {.val {out$warnings}}",
+      call = NULL
+    )
+  }
+
+  # regex shamelessly stolen from crayon::strip_style
+  ansi_regex <-"(?:(?:\\x{001b}\\[)|\\x{009b})(?:(?:[0-9]{1,3})?(?:(?:;[0-9]{0,3})*)?[A-M|f-m])|\\x{001b}[A-M]"
+  out <- gsub(ansi_regex, "", out$result, perl = TRUE, useBytes = TRUE) |>
+    paste0(collapse = "") |>
+    jsonlite::parse_json(simplifyVector = TRUE) |>
+    getElement("assets")
+
+  setDT(out)
+
+  ret <- out[,list(name, size, downloads = downloadCount, last_update = updatedAt, url)][
+    , size_string := as.character(rlang::as_bytes(size))
+  ]
+  ret <- ret[!grepl("timestamp", name)]
+
+  setDF(ret)
+  ret
+}
+
