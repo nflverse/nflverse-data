b12824e9567b1b4e568cb07cf28af3bb69b6019b
Tan <tan@tanho.ca>
2025-05-25 17:44:05 -0400

    add logger and specify verbose

--- dev/run_s3_archive.R
+++ dev/run_s3_archive.R
@@ -1,31 +1,36 @@
+logger::log_info("JOB START")
+options(verbose = TRUE)
 print(
   nflreadr::nflverse_sitrep(
     c("nflversedata", "nflreadr", "piggyback")
   )
 )
-
+logger::log_info("Downloading rds files from nflverse-data releases")
 purrr::walk(
   piggyback::pb_releases("nflverse/nflverse-data")$tag_name,
   \(x) nflversedata::.nflverse_download_assets(
     release_tag = x,
     file_type = ".rds",
-    download_dir = file.path("archives", "archive", format(Sys.Date()), "rds")
+    download_dir = file.path("archive", "archive", format(Sys.Date()), "rds")
   )
 )
 
+logger::log_info("Downloading parquet files from nflverse-data releases")
 purrr::walk(
   piggyback::pb_releases("nflverse/nflverse-data")$tag_name,
   \(x) nflversedata::.nflverse_download_assets(
     release_tag = x,
     file_type = ".parquet",
-    download_dir = file.path("archives", "archive", format(Sys.Date()), "parquet")
+    download_dir = file.path("archive", "archive", format(Sys.Date()), "parquet")
   )
 )
 
+logger::log_info("Syncing files to S3")
 aws.s3::s3sync(
-  path = "archives",
+  path = "archive",
   bucket = "nflverse",
   region = "",
-  direction = "upload",
+  verbose = TRUE,
   multipart = TRUE
 )
+logger::log_info("JOB COMPLETE")
